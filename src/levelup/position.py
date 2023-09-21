14c775ea506e62e28ca909d3bd83109642925827
Tim Davis <veridor@outlook.com>
2023-09-21 20:23:22 +0000

    added position.py class and test_position unit.py test

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,5 +1,8 @@
-class Point:
-    pass
-
-def Position():
-    pass
+class Position:
+    point = (0,0)
+    
+    def setPosition(self, x, y):
+        newPosition = list(self.point)
+        newPosition[0] = x
+        newPosition[1] = y
+        self.point = tuple(newPosition)
--- tests/test_position.py
+++ tests/test_position.py
@@ -1,8 +1,32 @@
-from unittest import TestCase
-from levelup.position import Point
+import unittest
+from levelup.position import Position
 
+class TestPosition(unittest.TestCase):
 
-def createPosition():
-    testPosition = self.Point
-    
+    def test_initialization(self):
+        testobj = Position()
+        expected_x_coordinate = 0
+        expected_y_coordinate = 0
+        self.assertEqual(
+            expected_x_coordinate,
+            testobj.point[0]
+        )
+        self.assertEqual(
+            expected_y_coordinate,
+            testobj.point[1]
+        )
+
+    def test_assignment(self):
+        testobj = Position()
+        assigned_x = 1
+        assigned_y = 1
+        testobj.setPosition(assigned_x, assigned_y)
+        self.assertEqual(
+            assigned_x,
+            testobj.point[0]
+        )
+        self.assertEqual(
+            assigned_y,
+            testobj.point[1]
+        )
 
