431b97f7ea0eea345389d768a7586fed85d7c44e
Tim Davis <veridor@outlook.com>
2023-09-22 15:16:15 +0000

    correcting faulty import of FakeMap in FakeCharacter (more burnt biscuits)

--- tests/fake_character.py
+++ tests/fake_character.py
@@ -1,4 +1,4 @@
-from levelup.map import FakeMap
+from test import FakeMap
 from levelup.character import Character
 from levelup.controller import Direction
 from levelup.position import Position
@@ -11,3 +11,10 @@ class FakeCharacter (Character):
 
     def __init__(self,character_name):
         self.current_position = Position (5,5)
+    
+    def move(self, direction: Direction) -> None:
+        self.is_move_called = True
+        self.last_move_direction = direction
+
+    def enter_map(self, map: Map) -> None:
+        self.is_enter_map_called = True
