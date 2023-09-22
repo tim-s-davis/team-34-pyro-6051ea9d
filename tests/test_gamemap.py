e75a01fdd4b2705bbe2b4f6b3612ac7dc683e8f7
Garrick <gqplaisted@gmail.com>
2023-09-22 13:43:55 +0000

    ADDED GAMEMAP AND TEST

---
+++ tests/test_gamemap.py
@@ -0,0 +1,104 @@
+from unittest import TestCase
+from levelup.character import Character
+#from levelup.controller import Direction
+from levelup.gamemap import GameMap
+from levelup.position import Position
+
+class TestMap(TestCase):
+    
+    def test_init_creates_positions(self):
+        testobj = GameMap()
+        self.assertNotEqual(None, testobj.positions)
+        self.assertEqual(10, len(testobj.positions))
+
+    def test_init_creates_positions_with_crrect_x_y(self):
+        testobj = GameMap()
+        self.assertEqual(3, testobj.positions[3][0].x)
+        self.assertEqual(7, testobj.positions[3][7].y)
+
+    def test_is_position_valid_when_true(self):
+        testobj = GameMap()
+        self.assertTrue(testobj.is_position_valid(Position(3,4)))
+
+    def test_is_position_valid_when_x_too_small(self):
+        testobj = GameMap()
+        self.assertFalse(testobj.is_position_valid(Position(-1,4)))
+
+    def test_is_position_valid_when_x_too_big(self):
+        testobj = GameMap()
+        self.assertFalse(testobj.is_position_valid(Position(10,4)))
+
+    def test_is_position_valid_when_y_too_small(self):
+        testobj = GameMap()
+        self.assertFalse(testobj.is_position_valid(Position(0,-1)))
+
+    def test_is_position_valid_when_y_too_big(self):
+        testobj = GameMap()
+        self.assertFalse(testobj.is_position_valid(Position(3,10)))
+
+    def test_is_position_valid_when_x_and_y_too_big(self):
+        testobj = GameMap()
+        self.assertFalse(testobj.is_position_valid(Position(11,10)))
+
+    def test_calculate_new_position_when_valid_NORTH(self):
+        testobj = GameMap()
+        startingPosition = Position(0,0)
+        expectedPosition = Position(0,1)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.NORTH)
+        self.assertEqual(expectedPosition, actualPosition)
+
+    def test_calculate_new_position_when_valid_SOUTH(self):
+        testobj = GameMap()
+        startingPosition = Position(3,3)
+        expectedPosition = Position(3,2)
+        actualPosition = testobj.calculate_new_position(
+            startingPosition, Direction.SOUTH)
+        self.assertEqual(expectedPosition, actualPosition)
+
+    def test_calculate_new_position_when_valid_EAST(self):
+        testobj = GameMap()
+        startingPosition = Position(3,3)
+        expectedPosition = Position(4,3)
+        actualPosition = testobj.calculate_new_position(
+            startingPosition, Direction.EAST)
+        self.assertEqual(expectedPosition, actualPosition)
+
+    def test_calculate_new_position_when_valid_WEST(self):
+        testobj = GameMap()
+        startingPosition = Position(3,3)
+        expectedPosition = Position(2,3)
+        actualPosition = testobj.calculate_new_position(
+            startingPosition, Direction.WEST)
+        self.assertEqual(expectedPosition, actualPosition)
+
+    def test_calculate_new_position_when_invalid_NORTH(self):
+        testobj = GameMap()
+        startingPosition = Position(9,9)
+        expectedPosition = Position(9,9)
+        actualPosition = testobj.calculate_new_position(
+            startingPosition, Direction.NORTH)
+        self.assertEqual(expectedPosition, actualPosition)
+
+    def test_calculate_new_position_when_invalid_SOUTH(self):
+        testobj = GameMap()
+        startingPosition = Position(0,0)
+        expectedPosition = Position(0,0)
+        actualPosition = testobj.calculate_new_position(
+            startingPosition, Direction.SOUTH)
+        self.assertEqual(expectedPosition, actualPosition)
+
+    def test_calculate_new_position_when_valid_EAST(self):
+        testobj = GameMap()
+        startingPosition = Position(9,9)
+        expectedPosition = Position(9,9)
+        actualPosition = testobj.calculate_new_position(
+            startingPosition, Direction.EAST)
+        self.assertEqual(expectedPosition, actualPosition)
+
+    def test_calculate_new_position_when_valid_WEST(self):
+        testobj = GameMap()
+        startingPosition = Position(0,0)
+        expectedPosition = Position(0,0)
+        actualPosition = testobj.calculate_new_position(
+            startingPosition, Direction.WEST)
+        self.assertEqual(expectedPosition, actualPosition)
